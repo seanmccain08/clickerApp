7b1e399bb367e0f46e6787716545dc69ce7c0de9
seanmccain08 <seanmccain327@gmail.com>
2024-09-25 07:24:38 -0500

    Initial Commit

---
+++ clickerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerApp
+//
+//  Created by SEAN MCCAIN on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ clickerAppTests/clickerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  clickerAppTests.swift
+//  clickerAppTests
+//
+//  Created by SEAN MCCAIN on 9/25/24.
+//
+
+import Testing
+@testable import clickerApp
+
+struct clickerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
