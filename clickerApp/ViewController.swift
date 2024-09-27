be182b197a8c75fdc37af5d279b24d75626ec9f4
seanmccain08 <seanmccain327@gmail.com>
2024-09-27 07:27:10 -0500

    complete

--- clickerApp/ViewController.swift
+++ clickerApp/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
+class AppData {
+    
+    static var points = 0
+    static var base = 1
+    static var multiplier = 1
+    static var basePrice = 20
+    static var multiplierPrice = 50
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tapAnywhereLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        pointsLabel.text = "\(AppData.points) Points"
+        
+    }
 
-
+    @IBAction func tapScreen(_ sender: Any) {
+        
+        AppData.points+=(AppData.base*AppData.multiplier)
+        pointsLabel.text = "\(AppData.points) Points"
+        
+        if tapAnywhereLabel.isHidden == false{
+            
+            tapAnywhereLabel.isHidden = true
+            
+        }
+        
+    }
+    
+    @IBAction func shopButton(_ sender: Any) {
+        
+        performSegue(withIdentifier: "shopSegue", sender: self)
+        
+    }
+    
 }
 
--- clickerApp/shopViewController.swift
+++ clickerApp/shopViewController.swift
@@ -9,21 +9,65 @@ import UIKit
 
 class shopViewController: UIViewController {
 
+    let alert = UIAlertController(title: "Insufficient Points", message: "You do not have enough points to purchase this.", preferredStyle: .alert)
+    
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var basePriceLabel: UILabel!
+    @IBOutlet weak var baseOwnedLabel: UILabel!
+    @IBOutlet weak var multiplierPriceLabel: UILabel!
+    @IBOutlet weak var multiplierOwnedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let button = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(button)
+        pointsLabel.text = "\(AppData.points) Points"
+        basePriceLabel.text = "Price: \(AppData.basePrice)"
+        baseOwnedLabel.text = "Current: \(AppData.base)"
+        multiplierPriceLabel.text = "Price: \(AppData.multiplierPrice)"
+        multiplierOwnedLabel.text = "Current: x\(AppData.multiplier)"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func baseUpgrade(_ sender: UIButton) {
+        
+        if(AppData.points >= AppData.basePrice){
+            
+            AppData.points -= AppData.basePrice
+            AppData.base += 1
+            AppData.basePrice*=2
+            basePriceLabel.text = "Price: \(AppData.basePrice)"
+            baseOwnedLabel.text = "Current: \(AppData.base)"
+            pointsLabel.text = "Points: \(AppData.points)"
+            
+        }
+        else{
+            
+            self.present(alert, animated: true)
+            
+        }
+        
     }
-    */
+    
+    @IBAction func multiplierUpgrade(_ sender: UIButton) {
+        
+        if(AppData.points >= AppData.multiplierPrice){
+            
+            AppData.points -= AppData.multiplierPrice
+            AppData.multiplier += 1
+            AppData.multiplierPrice*=2
+            multiplierPriceLabel.text = "Price: \(AppData.multiplierPrice)"
+            multiplierOwnedLabel.text = "Current: x\(AppData.multiplier)"
+            pointsLabel.text = "Points: \(AppData.points)"
+            
+        }
+        else{
+            
+            self.present(alert, animated: true)
+            
+        }
+        
+    }
 
 }
